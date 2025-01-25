09a14ec82020cfc536e9f5c60f56e4c949832539
TubaAreen-QA <tubaareen820@gmail.com>
2025-01-25 14:30:00 +0000

    add domain contracts for evaluation framework

---
+++ framework/models/__init__.py
@@ -0,0 +1,11 @@
+from .provider_request import ProviderRequest
+from .provider_response import ProviderResponse
+from .validation_result import ValidationResult
+from .evaluation_result import EvaluationResult
+
+__all__ = [
+    "provider_request",
+    "provider_response",
+    "validation_result",
+    "evaluation_result",
+]
